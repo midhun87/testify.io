d4bb1f983008c4b3c2563d4eedd154f55c571b6d
KOTAGIRI VENKATA SIVA SAI MIDHUN <159689456+midhun87@users.noreply.github.com>
2026-06-03 14:51:28 +0530

    Update sme-common.js

--- public/SME/sme-common.js
+++ public/SME/sme-common.js
@@ -40,6 +40,11 @@ function initializeSmePage(pageTitle) {
                 <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
                 Manage Aptitude Tests
             </a>
+
+              <a href="sme-create-public-test.html" class="sidebar-link flex items-center py-3 px-4 rounded-lg text-gray-300 hover:text-white" data-page="Public Tests">
+                <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
+                Public Tests
+            </a>
         `;
         
         // Set active link
